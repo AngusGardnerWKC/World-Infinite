a92e8399dd35dbe48f7841bc9781ec40da5f8444
Angus Gardner <32333714+AngusGardnerWKC@users.noreply.github.com>
2018-09-13 00:10:22 +0100

    Add files via upload

--- JS/changepage.js
+++ JS/changepage.js
@@ -1,14 +1,25 @@
 var images = ["JS/Prologue/page1.png", "JS/Prologue/page2.png", "JS/Prologue/page3.png"]
 
-x = 1
+x = 0
+document.getElementById('main-image').src = images[x];
 
-function changeImage() {
-
-	document.getElementById("main-image").src = images[x];
+function imageForward() {
 	x = x + 1
 	if(x > 2){
 		x = 0
 	}
+	document.getElementById("main-image").src = images[x];
+	
 
 }
 
+function imageBackward() {
+	x = x - 1
+	if(x < 0){
+		x = 2
+	}
+
+	document.getElementById("main-image").src = images[x];
+	
+
+}
