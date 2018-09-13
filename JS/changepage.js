53f91551d2f0fdf26e08793dc896cd85de1729f9
Angus Gardner <32333714+AngusGardnerWKC@users.noreply.github.com>
2018-09-13 14:22:37 +0100

    Add files via upload

--- JS/changepage.js
+++ JS/changepage.js
@@ -23,3 +23,36 @@ function imageBackward() {
 	
 
 }
+
+function getUrlParams( prop ) {
+    var params = {};
+    var search = decodeURIComponent( window.location.href.slice( window.location.href.indexOf( '?' ) + 1 ) );
+    var definitions = search.split( '&' );
+
+    definitions.forEach( function( val, key ) {
+        var parts = val.split( '=', 2 );
+        params[ parts[ 0 ] ] = parts[ 1 ];
+    } );
+
+    return ( prop && prop in params ) ? params[ prop ] : params;
+}
+
+pageNumber = getUrlParams("id");
+
+
+
+
+
+if (pageNumber == 1){
+	x  = 0;
+	document.getElementById('main-image').src = images[pageNumber - 1];
+}
+if (pageNumber == 2){
+	x = 1;
+	document.getElementById('main-image').src = images[pageNumber - 1];
+}
+if (pageNumber == 3){
+	x = 2;
+	document.getElementById('main-image').src = images[pageNumber - 1];
+}
+
